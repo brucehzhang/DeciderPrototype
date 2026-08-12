6fbd32d490e96edecfb0fb673e2edb50a99f0380
Bruce Zhang <brucehzhang@gmail.com>
2026-08-12 18:47:34 +0900

    initial commit

---
+++ src/main/java/com/tuning/deciderprototype/DeciderPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.tuning.deciderprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeciderPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeciderPrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuning/deciderprototype/DeciderPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuning.deciderprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeciderPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
