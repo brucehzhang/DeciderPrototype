87c3db2cd0d1d06ce48b02244e2e843ba4faffa5
Bruce Zhang <brucehzhang@gmail.com>
2026-08-12 21:26:11 +0900

    mcp client properties setup, tested basic gemini connectivity and tool check

--- src/main/java/com/tuning/deciderprototype/DeciderPrototypeApplication.java
+++ src/main/java/com/tuning/deciderprototype/DeciderPrototypeApplication.java
@@ -1,7 +1,12 @@
 package com.tuning.deciderprototype;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeciderPrototypeApplication {
@@ -10,4 +15,20 @@ public class DeciderPrototypeApplication {
         SpringApplication.run(DeciderPrototypeApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner predefinedQuestions(ChatClient.Builder chatClientBuilder, ToolCallbackProvider tools,
+                                                 ConfigurableApplicationContext context) {
+
+        return args -> {
+
+            var chatClient = chatClientBuilder
+                    .defaultTools(tools)
+                    .build();
+
+            System.out.println("\n>>> QUESTION: " + "What tools are available?");
+            System.out.println("\n>>> ASSISTANT: " + chatClient.prompt("What tools are available?").call().content());
+
+            context.close();
+        };
+    }
 }
