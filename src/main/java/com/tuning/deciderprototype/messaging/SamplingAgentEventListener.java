d6662fa0525a0de71bf77d9935d37741b4f01391
Bruce Zhang <brucehzhang@gmail.com>
2026-08-15 18:03:46 +0900

    switch to sqs listener

--- src/main/java/com/tuning/deciderprototype/messaging/SamplingAgentEventListener.java
+++ src/main/java/com/tuning/deciderprototype/messaging/SamplingAgentEventListener.java
@@ -2,26 +2,43 @@ package com.tuning.deciderprototype.messaging;
 
 import com.tuning.deciderprototype.models.SamplingAgentEvent;
 import com.tuning.deciderprototype.services.AgentDispatcherService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.function.Consumer;
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import org.springframework.stereotype.Service;
 
 // Kinesis event listener checking for any sampling that started, then resolves and dispatcher
-@Configuration
+@Service
 public class SamplingAgentEventListener {
 
-    @Bean
-    public Consumer<SamplingAgentEvent> samplingAgentEventConsumer(AgentDispatcherService agentDispatcherService) {
-        return event -> {
-            System.out.println("Received: " + event);
-            try {
-                agentDispatcherService.dispatch(event);
-            } catch (Exception e) {
-                // TODO:: Handle exceptions more gracefully
-                e.printStackTrace();
-                System.out.println("Exception occurred while trying to dispatch event: " + e.getMessage());
-            }
-        };
+    private final AgentDispatcherService _agentDispatcherService;
+
+    public SamplingAgentEventListener(AgentDispatcherService agentDispatcherService) {
+        _agentDispatcherService = agentDispatcherService;
+    }
+
+//    @Bean
+//    public Consumer<SamplingAgentEvent> samplingAgentEventConsumer(AgentDispatcherService agentDispatcherService) {
+//        return event -> {
+//            System.out.println("Received: " + event);
+//            try {
+//                agentDispatcherService.dispatch(event);
+//            } catch (Exception e) {
+//                // TODO:: Handle exceptions more gracefully
+//                e.printStackTrace();
+//                System.out.println("Exception occurred while trying to dispatch event: " + e.getMessage());
+//            }
+//        };
+//    }
+
+    @SqsListener("sampling_agent_event_queue")
+    public void listen(SamplingAgentEvent event) {
+        // TODO:: Improve error handling and logging
+        System.out.println("Received message " + event);
+        try {
+            _agentDispatcherService.dispatch(event);
+        } catch (Exception e) {
+            // TODO:: Handle exceptions more gracefully
+            e.printStackTrace();
+            System.out.println("Exception occurred while trying to dispatch event: " + e.getMessage());
+        }
     }
 }
